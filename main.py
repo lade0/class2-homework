a8dc4e709841085d9dd5b363c319ac7b36d239c8
lade0 <jesufemi33@yahoo.com>
2024-10-25 19:52:51 +0100

    initial

---
+++ main.py
@@ -0,0 +1,17 @@
+# storing 5 birthdays
+me=("14 august")
+other_person=(input("enter your date and month"))
+other_person2=(input("enter your date and month"))
+other_person3=(input("enter your date and month"))
+other_person4=(input("enter your date and month"))
+name=(input("enter your name"))
+name2=(input("enter your name"))
+name3=(input("enter your name"))
+name4=(input("enter your name"))
+print("my birthday is",me)
+print(name,"birthday is",other_person)
+print(name2,"birthday is",other_person2)
+print(name3,"birthday is",other_person3)
+print(name4,"birthday is",other_person4)
+
+14
